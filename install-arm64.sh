d4a61a48932863e557a8bdd6ce7e6ae6609a8cd8
Stefan Stanciulescu <contact@stefanstanciulescu.com>
2022-07-04 15:19:39 -0700

    Add install scripts and update README file

---
+++ install-arm64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/plausible/analytics
+
+cd analytics
+
+git checkout 3242327d
+
+docker build -t plausible/analytics . 
+
+cd ..
+
+git clone https://github.com/BytemarkHosting/docker-smtp
+
+cd docker-smtp/stretch && docker build -t bytemark/smtp . 
+
+docker-compose up -d
